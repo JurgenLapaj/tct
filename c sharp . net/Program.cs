4ac3b0da2b8934d7b04193217e7d1aeeea319f3c
Jurgen <jurgenlapaj3@gmail.com>
2023-02-14 20:25:19 +0100

    vazhdon detyra

--- c sharp . net/Program.cs
+++ c sharp . net/Program.cs
@@ -1,5 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using c_sharp_._net;
+
 Console.WriteLine("Hello, World!");
+
+
+
+funksioni_me_array_me_lista instanca = new funksioni_me_array_me_lista();
+
+
+
+Console.WriteLine("Ju lutem jepni nje array me int :");
+
+int n;
+Console.WriteLine("Sa doni te jete madhesia e array?");
+
+ n = Convert.ToInt32(Console.ReadLine());
+
+int[] array= new int[n]; 
+
+
+// kemi array te mbushue me element
+for (int i = 0;i < n; i++)
+{
+    Console.WriteLine("Jepni elementin ne pozicionin: " + i);
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+
+int shuma = instanca.GjejShumenEArray(array);
+int meIMadhi = instanca.GjejMeTeMadhin(array);
+
+
+
+
+
+Console.WriteLine("Shuma: " + shuma);
+Console.WriteLine("Me i madhi:" + meIMadhi);
+
 
 
 
