7d01f28b2588a5d2efda2b3b13c9469125a57041
Jurgen <jurgenlapaj3@gmail.com>
2023-02-21 20:34:11 +0100

    vazhdimi i msimit te ri

---
+++ c sharp . net/Helpers/PersonHelpers.cs
@@ -0,0 +1,32 @@
+﻿using c_sharp_._net.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_._net.Helpers
+{
+    internal class PersonHelpers
+    {
+        /// <summary>
+        /// Merr si imput Nje liste me persona 
+        /// </summary>
+        /// <param name="persons"></param>
+        /// <returns>Personat e renditur sipas moshes emrit dhe mbiemrit</returns>
+        public List<Person> RenditPersonSipasMoshes(List<Person> persons)
+        {
+            return persons.OrderBy(a =>a.Age).ThenBy (a => a.Name).ThenBy(a => a.Surname).ToList();
+        }
+
+        /// <summary>
+        /// Merr si imput nji list me persona
+        /// </summary>
+        /// <param name="persons"></param>
+        /// <returns>Kthen moshen mesatare te personave</returns>
+        public int GjejMoshenMesatare(List<Person> persons)
+        {
+            return persons.Average(a => a.Age);
+        }
+    }
+}
