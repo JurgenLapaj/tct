f4dec7b27f84654ea98a8e3f1aa6671759aa8b5a
Jurgen <jurgenlapaj3@gmail.com>
2023-02-21 19:33:48 +0100

    msim i ri

---
+++ c sharp . net/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_._net
+{
+    internal class Person
+    {
+        //private
+        private string _name;
+        private int age;
+        private string surname;
+        //protected
+        protected  int AgePublic;
+            protected string NamePublic;
+            protected string SurnamePublic;
+
+
+        //public
+
+        public string NamePublic;
+        public string SurnamePublic;
+        public int AgePublic;
+
+        public string NamePublic { get; set; }
+    }
+}
